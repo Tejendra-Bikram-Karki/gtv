0940f8ae0935683d07603e8cce3c5e7dbc164956
Pradeep Marasini <prodeepmarasini@gmail.com>
2022-08-09 14:49:59 +0545

    conflict solved

--- home-parts/popular.php
+++ home-parts/popular.php
@@ -10,13 +10,9 @@
         </div>
         <div class="col-md-2 d-flex flex-row-reverse ">
         <p>
-<<<<<<< HEAD
 				<span class="popular-span" ><img src="http://localhost/galaxy/wp-content/uploads/2022/08/Left-Nav-Btn.png" alt="" srcset=""></span>
 				<span class="popular-span ps-2" ><img src="http://localhost/galaxy/wp-content/uploads/2022/08/Right-Nav-Btn.png" alt="" srcset=""></span>
-=======
-				<span class="popular-span" style="font-size: 30px; cursor: pointer;">&#139;</span>
-				<span class="popular-span ps-5" style="font-size: 30px; cursor: pointer;">&#155;</span>
->>>>>>> 785a88f0423e852a178f4076cc0d5614c396daf6
+
 			</p>
         </div>
     </div>
--- nav.php
+++ nav.php
@@ -1,25 +1,3 @@
-
-<!-- <div class="row">
-    <div class="col-md-2">
-        <?php if ( function_exists( 'the_custom_logo' ) ) {
-                    the_custom_logo();
-                    }
-                    ?>
-    </div>
-    <div class="col-md-8 nav-menus">
-        <?php wp_nav_menu(
-            array(
-            'theme_location' => 'primary-menu',
-            'menu_class' => 'nav'
-            )
-            ); 
-            ?>
-    </div>
-    <div class="col-md-2"></div>
-
-</div> -->
-
-
 <nav class="navbar dark-background navbar-expand-lg sticky-top">
         <div class="container-fluid mx-5">
             <ul class="navbar-nav">
