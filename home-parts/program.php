94188042450313c64ba0d217cfdd4ab28793c041
Tejendra <karkitejendra33@gmail.com>
2022-08-09 21:17:26 +0545

    program h-me

--- home-parts/program.php
+++ home-parts/program.php
@@ -1,10 +1,10 @@
 <div class="container program-container my-5">
     <div class="row pb-4 pt-5">
         <div class="col-md-10">
-        <div class="title">
-        <p><i class="fa-solid fa-circle title-icon"></i></p>
-        <p class="who-we-are small-heading-dark">Programs</p>
-        </div>
+            <div class="title">
+                <p><i class="fa-solid fa-circle title-icon"></i></p>
+                <p class="who-we-are small-heading-dark">Programs</p>
+            </div>
             <!-- <p class="programs small-heading-dark"><i class="fa-solid fa-circle"></i>Programs</p> -->
         </div>
         <div class="col-md-2 d-flex flex-row-reverse ">
@@ -17,7 +17,7 @@
 
     <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-5 py-5">
 
-            <?php if( have_rows('program_section' ,23) ):
+        <?php if( have_rows('program_section' ,23) ):
             while( have_rows('program_section' ,23) ): the_row(); 
             ?>
 
@@ -27,7 +27,7 @@
                 <div class="overlay-small"></div>
                 <div class="card-img-overlay text-white program-text-content">
                     <h5 class="program-card-title">
-                    <?php the_sub_field('program_name', 23); ?>
+                        <?php the_sub_field('program_name', 23); ?>
                     </h5>
                     <p class="program-category"><?php the_sub_field('program_category', 23); ?></p>
                 </div>
@@ -37,4 +37,46 @@
         <?php endwhile; endif; ?>
 
     </div>
+
+
+
+
+    <!-- from program posts -->
+    <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-5 py-5">
+
+            <?php 
+            // $catData = get_queried_object();
+            $args = array(
+                'post_type'=>'programs', 
+                'post_status'=>'publish', 
+                'showposts'=>6
+                
+            );
+            // post_type and post_status for custom post types
+            $i = 0; 
+            $result = new WP_Query($args); 
+            while ($result -> have_posts() ) : $result -> the_post(); 
+            if($i++<4):
+            ?>
+
+        <div class="col">
+            <div class="card text-bg-dark h-100 single-program">
+                <?php the_post_thumbnail(); ?>
+                <div class="overlay-small"></div>
+                <div class="card-img-overlay text-white program-text-content">
+                    <h5 class="program-card-title">
+                       <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> 
+                    </h5>
+                    <p class="program-category">Category</p>
+                </div>
+            </div>
+        </div>
+
+             <?php  endif; endwhile;  wp_reset_postdata(); ?>
+
+    </div>
+
+
+
+
 </div>
